ff1284f18f272982b2fe3dcc2ed2d287190a738e
LEE.DH <dhlee318@me.com>
2019-08-06 17:16:54 +0900

    projects base

--- src/Projects.js
+++ src/Projects.js
@@ -1,12 +1,50 @@
 import React, { Component } from 'react';
+import { Typography, Grid } from '@material-ui/core';
+import ProjectList from './projects/ProjectList'
 
 class Projects extends Component {
+  static defaultProps = {
+    projects: [ {
+      'name':'Deep-Studio',
+      'img_dir': require ('./img/sample.png'),
+      'img_count': 31,
+      'comment':'Lorem ipsum dolor sit amet,consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
+    ,{
+      'name':'Pommelier',
+      'img_dir': require ('./img/sample.png'),
+      'img_count': 12,
+      'comment':'Lorem ipsum dolor sit amet,consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
+    ,{
+      'name':'DSC2019',
+      'img_dir': require ('./img/sample.png'),
+      'img_count': 12,
+      'comment':'Lorem ipsum dolor sit amet,consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
+    ,{
+      'name':'minor-market',
+      'img_dir': require ('./img/sample.png'),
+      'img_count': 12,
+      'comment':'Lorem ipsum dolor sit amet,consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
+    ,
+      ]
+    }
     render() {
-        return (
-          <section>
-            Projects
-          </section>
-        );
+      const { projects } = this.props;
+      const project_list = projects.map(
+        project => (<ProjectList key={project.index} name={project.name} comment={project.comment} img={project.img_dir}/>)
+      );
+      return (
+        <section>
+          <Typography variant='h1'>PROJECTS</Typography>
+            <Grid
+              container
+              direction="col"
+              justify="center"
+              alignItems="center"
+            >
+              {project_list}
+            </Grid>
+        </section>
+      );
     }
 }
 export default Projects;
---
+++ src/projects/ProjectList.js
@@ -0,0 +1,50 @@
+import React, { Component, Fragment } from 'react';
+import { Typography, Grid, Card, CardActionArea, CardActions, CardContent, CardMedia, Button,CardHeader } from '@material-ui/core';
+
+class ProjectList extends Component {
+    render() {
+      const name = this.props.name;
+      const comment = this.props.comment;
+      const img_dir = this.props.img;
+
+      return (
+        <Fragment>
+          <Grid item xs={12} sm={12} md={3}>
+            <Card>
+              <CardActionArea>
+                <CardHeader
+                  title={name}
+                  subheader="September 14, 2016"
+                />
+                <CardMedia
+                  style={{ height: 0, paddingTop: '100%'}}
+                  image={img_dir}
+                  title="Contemplative Reptile"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {name}
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    {comment}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button size="small" color="primary">
+                  Learn More
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        </Fragment>
+      );
+    }
+}
+// ProjectList.propTypes =  {
+//   'name': React.PropTypes.string.isRequired,
+//   'comment': React.PropTypes.string.isRequired,
+//   'img_dir': React.PropTypes.string.isRequired
+//
+// };
+export default ProjectList;
