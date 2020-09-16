13be54534e7ab3cf39e19438792ea640e06fa818
LEE.DH <dhlee318@me.com>
2020-09-17 02:00:54 +0900

    2020 하반기 작업 전 소개 문구 수정

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ import './scss/Header.scss'
 function Header() {
   return (
     <section className='header' id='Header' style={{backgroundImage: 'url("images/header.jpg")'}}>
-        <h1 className='resume-title'>더 나은 결과를 위해 최선을 다합니다.</h1>
+        <h1 className='resume-title'>호기심 많은 예비 개발자 이동현입니다.</h1>
     </section>
   );
 }
