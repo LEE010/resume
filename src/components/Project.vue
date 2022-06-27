bab3397c3a6ea3053a2c4d8df50f0f0d51eeabad
이동현 <mario@idonghyeon-ui-MacBookPro.local>
2022-06-28 00:30:53 +0900

    경력사항 UI 추가

---
+++ src/components/Project.vue
@@ -0,0 +1,34 @@
+<template>
+    <dl class="project_wrap" v-bind:class="{ on: isActive }">
+      <dt class="project_title">{{project.title}}</dt>
+      <button class="btn_project_toggle" @click="isActive=!isActive">
+          <span class="blind">열기</span>
+      </button>
+      <dd class="project_content">
+          <div class="project_dates">
+              <span class="date">{{project.start}}</span><span class="date">{{project.end}}</span>
+          </div>
+          <div class="project_duty">
+              {{project.duty}}
+          </div>
+          <p class="project_summary">
+              {{project.summary}}
+          </p>
+      </dd>
+  </dl>
+</template>
+
+<script>
+export default {
+  name: 'Project',
+  props: {
+    projects: Array,
+    project: Object
+  },
+  data: function () {
+    return {
+      isActive: false
+    }
+  }
+}
+</script>
---
+++ src/components/WorkContent.vue
@@ -0,0 +1,29 @@
+<template>
+    <li class="exp">
+        <div class="exp_date">
+            <div class="date_start"><span class="date">{{work.start}}</span></div>
+            <div class="date_end"><span class="date">{{work.end}}</span></div>
+        </div>
+        <div class="exp_info">
+            <strong class="exp_head">{{ work.name }}</strong>
+            <div class="exp_subhead">{{ work.subname }}</div>
+            <p class="exp_detail">{{ work.content }}</p>
+            <projects v-if="work.projects.length > 0" :projects="work.projects"/>
+        </div>
+    </li>
+</template>
+
+<script>
+import Projects from '@/components/Projects'
+
+export default {
+  name: 'WorkContent',
+  components: {
+    Projects
+  },
+  props: {
+    work: Object,
+    projects: Array
+  }
+}
+</script>
