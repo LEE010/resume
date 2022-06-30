334d08116dd100dd4982b95e0f2964aeb8b4e207
이동현 <mario@idonghyeon-ui-MacBookPro.local>
2022-07-01 00:14:52 +0900

    경력 항목 구현

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -6,14 +6,30 @@
       </button>
       <dd class="project_content">
           <div class="project_dates">
-              <span class="date">{{project.start}}</span><span class="date">{{project.end}}</span>
-          </div>
-          <div class="project_duty">
-              {{project.duty}}
+              <span class="project_date">{{project.start}}</span>
+              <span class="project_date">{{project.end}}</span>
           </div>
           <p class="project_summary">
               {{project.summary}}
           </p>
+          <div class="project_skills_wrap">
+              <span class="project_skills_head">주요기술</span>
+              <ul class="project_skills">
+                  <li class="project_skill" :class="'logo_' + skill" v-for="skill in project.skills" :key="skill">
+                      <span class="blind">{{ skill }}</span>
+                  </li>
+              </ul>
+          </div>
+          <ul class="project_contents">
+              <li class="project_content_detail" v-for="(content, head) in project.contents" :key="head">
+                  <span class="project_content_head">{{ head }}</span>
+                  <div class="project_content_item_wrap">
+                      <p class="project_content_item" v-for="(p, idx) in content.split('\n')" :key="idx">
+                          {{ p }}
+                      </p>
+                  </div>
+              </li>
+          </ul>
       </dd>
   </dl>
 </template>
