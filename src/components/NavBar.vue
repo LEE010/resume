39dcac6f7cb6f8617d0cdd3ee1ef15a06de01752
이동현 <mario@idonghyeon-ui-MacBookPro.local>
2022-07-02 07:25:32 +0900

    이력서 제목 변경 및 navbar 누락 링크 추가

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,12 +2,13 @@
     <header class="resume_header">
         <h1 class="page_title">
             <router-link to="/" class="page_logo">
-                <span class="logo_txt">LDH's Web Portfolio.</span>
+                <span class="logo_txt">신입 개발자 이동현의 이력서</span>
             </router-link>
         </h1>
         <nav class="page_nav">
             <ul class="nav_items">
                 <li class="nav_item"><a href="#about" class="nav_link">About</a></li>
+                <li class="nav_item"><a href="#works" class="nav_link">Works</a></li>
                 <li class="nav_item"><a href="#exps" class="nav_link">Exps</a></li>
                 <li class="nav_item"><a href="#contact" class="nav_link">Contact</a></li>
             </ul>
