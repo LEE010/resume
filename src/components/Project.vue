ad1e9113d37bd010c8940ed494f24af2904c2d87
이동현 <mario@idonghyeon-ui-MacBookPro.local>
2022-07-08 00:09:31 +0900

    프로젝트 상세 컴포넌트 드롭다운 애니메이션 처리 개선

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,37 +1,50 @@
 <template>
     <dl class="project_wrap" v-bind:class="{ on: isActive }">
-      <dt class="project_title">{{project.title}}</dt>
-      <button class="btn_project_toggle" @click="isActive=!isActive">
-          <span class="blind">열기</span>
-      </button>
-      <dd class="project_content">
-          <div class="project_dates">
-              <span class="project_date">{{project.start}}</span>
-              <span class="project_date">{{project.end}}</span>
-          </div>
-          <p class="project_summary">
-              {{project.summary}}
-          </p>
-          <div class="project_skills_wrap">
-              <span class="project_skills_head">주요기술</span>
-              <ul class="project_skills">
-                  <li class="project_skill" :class="'logo_' + skill" v-for="skill in project.skills" :key="skill">
-                      <span class="blind">{{ skill }}</span>
-                  </li>
-              </ul>
-          </div>
-          <ul class="project_contents">
-              <li class="project_content_detail" v-for="(content, head) in project.contents" :key="head">
-                  <span class="project_content_head">{{ head }}</span>
-                  <div class="project_content_item_wrap">
-                      <p class="project_content_item" v-for="(p, idx) in content.split('\n')" :key="idx">
-                          {{ p }}
-                      </p>
-                  </div>
-              </li>
-          </ul>
-      </dd>
-  </dl>
+        <dt class="project_title">
+            {{ project.title }}
+            <button class="btn_project_toggle" @click="isActive = !isActive">
+                <span class="blind">열기</span>
+            </button>
+        </dt>
+        <Transition name="slide"
+            @before-enter="beforeEnter"
+            @enter="enter"
+            @before-leave="beforeLeave"
+            @leave="leave"
+        >
+            <dd class="project_content" v-show="isActive">
+                <div class="project_dates">
+                    <span class="project_date">{{ project.start }}</span>
+                    <span class="project_date">{{ project.end }}</span>
+                </div>
+                <p class="project_summary">
+                    {{ project.summary }}
+                </p>
+                <div class="project_skills_wrap">
+                    <span class="project_skills_head">주요기술</span>
+                    <ul class="project_skills">
+                        <li class="project_skill"
+                            v-for="skill in project.skills"
+                            :class="'logo_' + skill"
+                            :key="skill"
+                        >
+                            <span class="blind">{{ skill }}</span>
+                        </li>
+                    </ul>
+                </div>
+                <ul class="project_contents">
+                    <li class="project_content_detail" v-for="(content, head) in project.contents" :key="head">
+                        <span class="project_content_head">{{ head }}</span>
+                        <div class="project_content_item_wrap">
+                            <p class="project_content_item" v-for="(p, idx) in content.split('\n')" :key="idx">
+                                {{ p }}
+                            </p>
+                        </div>
+                    </li>
+                </ul>
+            </dd>
+        </Transition>
+    </dl>
 </template>
 
 <script>
@@ -44,6 +57,20 @@ export default {
   data: function () {
     return {
       isActive: false
+    }
+  },
+  methods: {
+    beforeEnter (element) {
+      element.style.height = 0
+    },
+    enter (element) {
+      element.style.height = `${element.scrollHeight}px`
+    },
+    beforeLeave (element) {
+      element.style.height = `${element.scrollHeight}px`
+    },
+    leave (element) {
+      element.style.height = 0
     }
   }
 }
